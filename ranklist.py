c8f37ed072ca9f124925f84eb4b52eccc543c86d
LazyCoder-1506 <pranayan1506@gmail.com>
2021-08-30 15:35:56 +0530

    complete

--- ranklist.py
+++ ranklist.py
@@ -8,6 +8,28 @@ rank_wb = Workbook()
 
 branch = 'AA'
 
-for row in range(1, 14):
+rlist = []
+
+for row in range(1, 1367):
   rollno = ori_ws['A' + str(row)].value
+  dep = rollno[2:4]
+  student = ori_ws['B' + str(row)].value
+  cg = float((ori_ws['C' + str(row)].value).split(' ')[0])
+  data = [cg, rollno, student]
+  if not dep == branch:
+    rlist.sort(reverse=True)
+    rank_wb.create_sheet(branch)
+    thisdep = rank_wb[branch]
+    thisdep.append(['Rank', 'Roll No.', 'Name', 'CGPA'])
+    for rank in range(1, len(rlist) + 1):
+      data = [rank, rlist[rank-1][1], rlist[rank-1][2], rlist[rank-1][0]]
+      thisdep.append(data)
+
+    branch = dep
+    rlist = []
   
+  record = [cg, rollno, student]
+  rlist.append(record)
+
+rank_wb.save('Dep-RankList.xlsx')
+print('Done')
